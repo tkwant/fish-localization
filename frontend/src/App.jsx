0b0d006f045ebc411a70a357ef8ed582630b74e6
tkwant <t.kwant1@gmail.com>
2021-06-12 20:09:53 +0200

    remove old code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react'
-import Dashboard from './screens/Dashboard/Dashboard'
 import VideoPage from './screens/VideoPage/VideoPage'
 import FrontPage from './screens/FrontPage/FrontPage'
 import { ChakraProvider } from '@chakra-ui/react'
@@ -7,27 +6,27 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { extendTheme } from "@chakra-ui/react"
 import Title from './Title'
-const theme = extendTheme({
-    fonts: {
-        heading: "Inter",
-        body: "Inter",
-    },
-})
+// const theme = extendTheme({
+//     fonts: {
+//         heading: "Inter",
+//         body: "Inter",
+//     },
+// })
 const queryClient = new QueryClient()
 const App = () => {
     return (
         <QueryClientProvider client={queryClient}>
-            <ChakraProvider theme={theme}>
-                <div class="flex flex-col h-screen">
-                    <Title />
-                    <BrowserRouter>
-                        <Switch>
-                            <Route path="/" component={FrontPage} exact />
-                            <Route path="/player" component={VideoPage} />
-                        </Switch>
-                    </BrowserRouter>
-                </div>
-            </ChakraProvider>
+            {/* <ChakraProvider theme={theme}> */}
+            <div class="flex flex-col h-screen">
+                <Title />
+                <BrowserRouter>
+                    <Switch>
+                        <Route path="/" component={FrontPage} exact />
+                        <Route path="/player" component={VideoPage} />
+                    </Switch>
+                </BrowserRouter>
+            </div>
+            {/* </ChakraProvider> */}
         </QueryClientProvider>
     )
 }
--- frontend/src/screens/Dashboard/Card.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import Sidebar from './SidebarCards'
-
-const Card = () => {
-    return <div>Card</div>
-}
-
-export default Card
